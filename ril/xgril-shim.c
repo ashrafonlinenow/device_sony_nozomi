68307539f4a01a018e27fafa70d6df679d1ad777
Milos Ratkovic <milosr@gmail.com>
2016-10-05 21:15:00 +0200

    added hex dump to ril shim

--- ril/xgril-shim.c
+++ ril/xgril-shim.c
@@ -5,6 +5,16 @@
 #include <unwind.h>
 #include <dlfcn.h>
 
+#define DEBUG 1
+
+#if DEBUG
+#define hexDump(desc,addr,len) hexDumpImpl(desc,addr,len)
+#else 
+#define hexDump(desc,addr,len)
+#endif
+
+static void hexDumpImpl (char *desc, void *addr, int len);
+
 /* A copy of the original RIL function table. */
 static const RIL_RadioFunctions *origRilFunctions;
 
@@ -24,6 +34,7 @@ static void onRequestCompleteShim(RIL_Token t, RIL_Errno e, void *response, size
 
 	request = pRI->pCI->requestNumber;
     RLOGD("onRequestCompleteShim: %s", requestToString(request));
+    hexDump(NULL, response, responselen);
 
 	switch (request) {
 		case RIL_REQUEST_GET_SIM_STATUS:
@@ -57,7 +68,7 @@ static void onRequestCompleteShim(RIL_Token t, RIL_Errno e, void *response, size
 	RLOGD("%s: got request %s: forwarded to libril.\n", __func__, requestToString(request));
 null_token_exit:
 	rilEnv->OnRequestComplete(t, e, response, responselen);
-    RLOGD("onRequestCompleteShim - finished");
+    RLOGD("onRequestCompleteShim %s - finished", requestToString(request));
 }
 
 #define ssprintf(pBuf, ...) pBuf += sprintf(pBuf, __VA_ARGS__)
@@ -82,7 +93,7 @@ static _Unwind_Reason_Code unwindCallback(struct _Unwind_Context* context, void*
     return _URC_NO_REASON;
 }
 
-size_t captureBacktrace(void** buffer, size_t max)
+static size_t captureBacktrace(void** buffer, size_t max)
 {
     BacktraceState state = {buffer, buffer + max};
     _Unwind_Backtrace(unwindCallback, &state);
@@ -90,7 +101,7 @@ size_t captureBacktrace(void** buffer, size_t max)
     return state.current - buffer;
 }
 
-void logBacktrace(void** buffer, size_t count)
+static void logBacktrace(void** buffer, size_t count)
 {
     char buff[16384];
     char *pBuff = buff;
@@ -108,13 +119,76 @@ void logBacktrace(void** buffer, size_t count)
     RLOGE("%s", buff);
 }
 
-void sigHandler(int sig) {
+static void hexDumpImpl (char *desc, void *addr, int len) {
+    int i;
+    unsigned char buff[17];
+    unsigned char *pc = (unsigned char*)addr;
+    char sbuff[8192];
+    char *sbp = sbuff;
+
+    // Output description if given.
+    if (desc != NULL)
+        ssprintf(sbp, "%s:\n", desc);
+
+    if (len == 0) {
+        ssprintf(sbp, "  ZERO LENGTH\n");
+        return;
+    }
+    if (len < 0) {
+        ssprintf(sbp, "  NEGATIVE LENGTH: %i\n",len);
+        return;
+    }
+
+    // Process every byte in the data.
+    for (i = 0; i < len; i++) {
+        // Multiple of 16 means new line (with line offset).
+
+        if ((i % 16) == 0) {
+            // Just don't print ASCII for the zeroth line.
+            if (i != 0)
+                ssprintf(sbp, "  %s\n", buff);
+
+            // Output the offset.
+            ssprintf(sbp, "  %04x ", i);
+        }
+
+        // Now the hex code for the specific character.
+        ssprintf(sbp, " %02x", pc[i]);
+
+        // And store a printable ASCII character for later.
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
+            buff[i % 16] = '.';
+        else
+            buff[i % 16] = pc[i];
+        buff[(i % 16) + 1] = '\0';
+    }
+
+    // Pad out last line if not exactly 16 characters.
+    while ((i % 16) != 0) {
+        ssprintf(sbp, "   ");
+        i++;
+    }
+
+    // And print the final ASCII bit.
+    ssprintf(sbp, "  %s\n", buff);
+
+    RLOGD("%s", sbuff);
+}
+
+static void sigHandler(int sig) {
     const size_t max = 50;
     void* buffer[max];
 
     RLOGE("Error: signal %d", sig);
     logBacktrace(buffer, captureBacktrace(buffer, max));
     exit(EXIT_FAILURE);
+}
+
+static void onRequestShim (int request, void *data, size_t datalen, RIL_Token t) {
+    RLOGD("onRequestShim: %s", requestToString(request));
+    hexDump(NULL, data, datalen);
+    
+    origRilFunctions->onRequest(request, data, datalen, t);
 }
 
 const RIL_RadioFunctions* RIL_Init(const struct RIL_Env *env, int argc, char **argv)
@@ -124,8 +198,9 @@ const RIL_RadioFunctions* RIL_Init(const struct RIL_Env *env, int argc, char **a
 	static struct RIL_Env shimmedEnv;
 	void *origRil;
 
-    RLOGI("RIL_Init, registering SIGABRT handler");
+    RLOGI("RIL_Init, registering SIGABRT& SIGSEGV handler");
     signal(SIGABRT, sigHandler);
+    signal(SIGSEGV, sigHandler);
 
 	/* Shim the RIL_Env passed to the real RIL, saving a copy of the original */
 	rilEnv = env;
@@ -154,6 +229,9 @@ const RIL_RadioFunctions* RIL_Init(const struct RIL_Env *env, int argc, char **a
 
 	/* Shim functions as needed. */
 	shimmedFunctions = *origRilFunctions;
+#if DEBUG	
+	shimmedFunctions.onRequest = onRequestShim;
+#endif
 
 	return &shimmedFunctions;
 
